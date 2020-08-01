38e7102f09c748ce1e875af85382778459430bdc
naechrist <729jenae.christensen@gmail.com>
2020-08-01 13:58:08 -0400

    Done.

---
+++ app/controllers/coupons_controller.rb
@@ -0,0 +1,15 @@
+class CouponsController < ApplicationController 
+    def index
+    end 
+
+    def new
+    end 
+
+    def create
+        coupon = Coupon.new
+        coupon.coupon_code = params[:coupon_code]
+        coupon.store = params[:store]
+        coupon.save 
+        redirect_to Coupon.last
+    end 
+end 
